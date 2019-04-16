5ff9aaa48e753d69204ebab3a3db1adfa8718af8
Jiten Vij <jitenvij1992@gmail.com>
2019-04-16 16:10:08 +0530

    initial commit of full repository

---
+++ src/test/java/com/docker/DockerCompose/DockerComposeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.docker.DockerCompose;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerComposeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
